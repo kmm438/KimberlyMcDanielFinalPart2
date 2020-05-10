fd7262e712076824ffbfd542521adcdf381699da
Kimberly McDaniel <km438@njit.edu>
2020-05-10 15:19:38 +0100

    Feature: Updated the landing screen template to display heights and weights in a more tidy way

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Cities Project'}
+    user = {'username': 'Final Project Part 1'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM bmiData')
     result = cursor.fetchall()
