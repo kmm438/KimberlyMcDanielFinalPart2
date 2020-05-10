a70c7131bce4d9826cf0a9988d34372c12975dde
Kimberly McDaniel <km438@njit.edu>
2020-05-10 16:26:25 +0100

    Feature: Added the feature for adding new entries to the db.

--- app/app.py
+++ app/app.py
@@ -55,13 +55,11 @@ def form_insert_get():
     return render_template('new.html', title='New BMI Form')
 
 
-@app.route('/cities/new', methods=['POST'])
+@app.route('/bmi/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'))
-    sql_insert_query = """INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    inputData = (request.form.get('Height_Inches'), request.form.get('Weight_Pounds'))
+    sql_insert_query = """INSERT INTO bmiData (Index_HW,Height_Inches,Weight_Pounds) VALUES ('1', %s, %s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
